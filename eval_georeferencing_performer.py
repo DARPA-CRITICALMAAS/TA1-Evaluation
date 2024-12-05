e329d18b1a7526362934e11788da053e049dbf2f
adardas <adardas@mitre.org>
2024-12-05 10:54:11 -0500

    Add files via upload

---
+++ eval_georeferencing_performer.py
@@ -0,0 +1,47 @@
+from etls.georeferencing import PreEvalGeoref, GeorefEval
+from pandas import read_csv
+
+if __name__ == "__main__":
+    """
+    16 CPU machine => 22 seconds. 
+    """
+
+    tif_files = "data/ground_truth/georef_tif_files.parquet"
+    eval_dict = "data/ground_truth/cog_id_info.json"
+
+    georef_eval_gcps = PreEvalGeoref(tif_files=tif_files,
+                                     eval_df=eval_dict)
+
+    # Change here
+    cdr_token = "---Add your token here ---"
+    output_dir = "data/inferenced_cdr/Georeferencing"
+    cdr_systems = {
+        "annotated": [{"system": "ngmdb", "system_version": "2.0"},
+                      {"system": "polymer", "system_version": "0.0.1"}],
+        "performers": [{"system": "uncharted-georeference", "system_version": "0.0.6"},
+                       {"system": "umn-usc-inferlink", "system_version": "0.0.7"}]
+    }
+
+    """
+    16 CPU machine => 11 seconds. 
+    """
+    georef_metrics = GeorefEval(cdr_token=cdr_token,
+                                georef_dataset=georef_eval_gcps.concat_df,
+                                output_dir=output_dir,
+                                cdr_systems=cdr_systems)
+
+    # failed_cogs_download = georef_metrics.failed_download
+    rmse_data = georef_metrics.concat_df
+    # rmse_data.to_parquet(f"{output_dir}/rmse_metrics_georeferencing.parquet")
+
+    #
+    eval_df = read_csv("data/eval_final_set.csv")
+
+    augment_data = (
+        eval_df
+        .merge(rmse_data, left_on=['COG ID'], right_on=['cog_id'])
+    )
+
+    # augment_data = rmse_data.merge(data.inventory_data.eval_df, left_on=['cog_id'], right_on=['COG ID'])
+    # augment_data.to_csv("data/final_metrics/georeferencing_rmse_12_month.csv", index=False)
+    # rmse_data_missing = rmse_data.query('log == "annotated-missing"')
